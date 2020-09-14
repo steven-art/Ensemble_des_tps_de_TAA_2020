601f747517e994708a15549bfa375d26161bb0a7
steven-art <tanguysteven15@gemail.com>
2020-09-14 19:32:26 +0200

    TP2 de TAA en réalisation

---
+++ eclipse-workspace/Tp2/src/test/java/fr/istic/m2/steven/taa/tp/Tp2/AppTest.java
@@ -0,0 +1,20 @@
+package fr.istic.m2.steven.taa.tp.Tp2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
